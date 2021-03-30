65449263e7a8c7adaf6c9c5b37c54a6588096729
RyutaKojima <rkojima0213@gmail.com>
2021-03-30 10:37:46 +0900

    システム通知を追加

--- dist/js/background_script.js
+++ dist/js/background_script.js
@@ -1,19 +1,56 @@
+let attendanceTime = null;
 let timeoutID = null;
 
-chrome.runtime.onMessage.addListener((message, MessageSender, sendResponse) => {
+const notifyAkashiTime = () => {
+    const opt = {
+        type: 'basic',
+        title: '退勤時間になりました',
+        message: '忘れずに AKASHI を入力しましょう',
+        iconUrl: '../img/icon_48.png'
+    };
+
+    chrome.notifications.create('', opt);
+}
+
+const openAkashiTab = () => {
+    chrome.tabs.create({
+        url: 'https://atnd.ak4.jp/',
+        active: true
+    });
+}
+
+const registerAttendance = () => {
     if (timeoutID) {
         clearTimeout(timeoutID)
         timeoutID = null;
     }
 
+    attendanceTime = new Date();
     timeoutID = setTimeout(() => {
-        timeoutID = null;
+            timeoutID = null;
+            attendanceTime = null;
 
-        chrome.tabs.create({
-            url: 'https://atnd.ak4.jp/',
-            active: true
-        });
-    }, 9 * 60 * 60 * 1000);
+            openAkashiTab()
+            notifyAkashiTime();
+        },
+        9 * 60 * 60 * 1000
+    );
 
-    sendResponse({'message': 'ok'});
+    return {
+        attendance: attendanceTime.toLocaleString('ja-JP')
+    }
+}
+
+chrome.runtime.onMessage.addListener((message, MessageSender, sendResponse) => {
+    let response = {}
+
+    switch (message.action) {
+        case 'attendance':
+            response = registerAttendance();
+            break;
+        default:
+            response = {error: 'Unknown action'}
+    }
+
+    sendResponse({'message': 'ok', ...response});
 });
